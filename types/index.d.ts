ffae0b58284e9c0df62eaf92ad1698992ddcc5c5
Tom <tomklaversma@gmail.com>
2020-11-04 11:54:15 +0100

    ComposedPredicate based on Tuple type

--- types/index.d.ts
+++ types/index.d.ts
@@ -4,9 +4,19 @@ interface AuthClient {
 
 type AuthToken = AuthClient | string;
 
+type ArgumentTypes<T extends (...a: any) => any> = T extends (
+  ...a: infer A
+) => any
+  ? A
+  : never;
+
+type ExpandTuple<T1, T2 extends any[]> = ArgumentTypes<
+  (a: T1, ...r: T2) => void
+>;
+
 type Predicate = true | [string, string, string];
 
-type ComposedPredicate = [string, ...Predicate[]];
+type ComposedPredicate = ExpandTuple<string, Predicate[]>;
 
 interface Entity {
   syncContent(): Promise<void>;
