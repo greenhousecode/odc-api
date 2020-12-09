0230d5a95c96ebaeae3d53be58abfad334d7504b
Tom <tomklaversma@gmail.com>
2020-12-09 12:25:09 +0100

    Added hasChanges status

--- src/entities/Adset.ts
+++ src/entities/Adset.ts
@@ -56,6 +56,8 @@ export default class Adset implements Entity {
 
   public variants: VariantsConfig;
 
+  public hasChanges = false;
+
   constructor(
     private client: ODC,
     private adsetId: number,
@@ -199,6 +201,8 @@ export default class Adset implements Entity {
     });
 
     this.content.data.rules.push(rule);
+
+    this.hasChanges = true;
   }
 
   removeContextRuleByPredicate(predicate: Predicate | ComposedPredicate) {
@@ -211,12 +215,16 @@ export default class Adset implements Entity {
       return this.content.data.rules.splice(index, 1)[0];
     }
 
+    this.hasChanges = true;
+
     return null;
   }
 
   removeAllContextRules() {
     const defaults = this.content.data.rules.shift();
     this.content.data.rules = [defaults];
+
+    this.hasChanges = true;
   }
 
   getContextRuleByPredicate(predicate: Predicate | ComposedPredicate) {
@@ -246,6 +254,8 @@ export default class Adset implements Entity {
       expr: defaultValue,
       name: placeholder.name,
     });
+
+    this.hasChanges = true;
   }
 
   removePlaceholder(placeholder: Placeholder) {
@@ -262,6 +272,8 @@ export default class Adset implements Entity {
       this.content.data.rules[0].assignments.splice(assignmentIndex, 1);
       return this.content.data.placeholders.splice(placeholderIndex, 1)[0];
     }
+
+    this.hasChanges = true;
 
     return null;
   }
